533bcecd210cd4a8605c9fdde06af0ace9d38d2d
Dilip Agheda <dilip_agheda@yahoo.com>
2020-08-08 11:46:12 +1000

    Added required endpoints and env configuration in node.js server side

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,54 @@
+const axios = require('axios');
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser');
+var cors = require('cors')
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.API_KEY)
+
+const API_KEY = process.env.API_KEY
 
 const app = express()
 
-app.use(express.static('dist'))
+app.use(cors())
 
-console.log(__dirname)
+app.use(express.static(path.resolve('../../dist')))
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+// console.log(path.resolve('../../dist'))
+// console.log(__dirname)
+
+app.get('/sentiment',  function (req,res) {
+    
+    console.log(req.query);
+
+    const baseUrl = `https://api.meaningcloud.com/sentiment-2.1`;
+    const params = {
+        key: 'c9828b58e373cf98ebf222be70c2dc2b',
+        lang: 'en',
+        txt: req.query.text
+    }
+    
+    axios({
+        url: baseUrl,
+        method: 'post',
+        params: params
+    })
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+ 
+
+    
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
