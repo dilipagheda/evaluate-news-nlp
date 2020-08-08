df158e43f637a258692345756dbe770f37dbabb1
Dilip Agheda <dilip_agheda@yahoo.com>
2020-08-08 21:29:48 +1000

    Added service worker in prod config and installed packages for jest, service worker workbox

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -30,6 +31,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),        
-        new MiniCssExtractPlugin({filename: '[name].[contentHash].css'})
+        new MiniCssExtractPlugin({filename: '[name].[contentHash].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
